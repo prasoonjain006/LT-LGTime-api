9e9e6ef50eacac25b906b1561b16b5e797d36145
prasoonjain006 <prasoonjain006@gmail.com>
2021-05-26 09:36:33 +0530

    initial commit

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ const BodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
 const { Server, ReplSet } = require("mongodb");
+
+const cors = require('cors');
 var app = Express();
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 
 
