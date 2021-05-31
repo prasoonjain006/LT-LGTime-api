0e2eba2e217937df6ffc98dc3f72c35842a82858
prasoonjain006 <prasoonjain006@gmail.com>
2021-05-31 21:45:35 +0530

    add login api

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ var app = Express();
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 app.use(cors());
+const User = require('./User');
+const jwt =require('jsonwebtoken');
 
 
 
@@ -46,6 +48,93 @@ app.use(cors());
                     response.send(result);
                 });
         });
+
+
+        app.post('/auth', (req,res)=>{
+            const {email,password} = req.body;
+            User.findOne({email},(err,user)=>{
+                if(err){
+                    res.status(500).send("Internal error");
+                }else if(!user){
+                    res.status(401).send("Incorect email");
+                }else{
+                    user.isCorrectPassword(password,(err,right)=>{
+                        if(err){
+                            res.status(500).send("Internal error");
+                        }else if(!right){
+                            console.log(right);
+                            res.status(401).send("Incorect email or password");
+                        }else {
+                            // Issue token
+                            const payload = { email };
+                            const token = jwt.sign(payload, secret, {
+                              expiresIn: '1h'
+                            });
+                            console.log("tokenLL",token);
+                           // res.cookie('token', token, { httpOnly: true }).sendStatus(200);
+                           res.json({
+                            token: token
+                            });
+                        }
+        
+                    })
+                }
+            })
+           
+        })
+
+
+
+        const chkToken = (req,res,next)=>{
+            const token =  req.body.token ||
+            req.query.token ||
+            req.headers['x-access-token'] 
+            console.log("DD",token)
+            if(!token){
+                res.status(401).send('Unauthorised');
+            }else{
+                console.log("hre",token)
+                jwt.verify(token,secret,(err,success)=>{
+                    console.log("error",err);
+                    if(err){
+                        res.status(401).send('Unauthorised');
+                    }else{
+                        req.email = success.email;
+                        next();
+                    }
+        
+                })
+            }
+        }
+        app.get('/checktoken',chkToken,(req,res)=>{
+            res.sendStatus(200);
+        })
+        
+        app.post('/register', (req,res)=>{
+            const {email,password} = req.body;
+            let user = new User({email,password});
+            user.save((err)=>{
+                if(err){
+                    res.status(500).send("Internal error");
+                }
+                else{
+                    res.status(200).send("Inserted");
+                }
+            })
+        })
+        
+        app.get('/secret', chkToken, function(req, res) {
+            res.send('Welcome to Secret Component from Server');
+          });
+
+
+
+
+
+
+
+
+
      }catch(e){
          console.log(e);
      }
